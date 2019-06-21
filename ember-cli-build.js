da9ca02b38bc59e1d11489547bb4fa9a5edf2559
Ryan Toronto <ryanto@gmail.com>
2019-06-21 13:52:52 -0400

    Fix uglify deploy error

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -3,7 +3,16 @@
 const EmberAddon = require('ember-cli/lib/broccoli/ember-addon');
 
 module.exports = function(defaults) {
-  let app = new EmberAddon(defaults, {});
+  let app = new EmberAddon(defaults, {
+    // Workaround for https://github.com/ember-cli/ember-cli/issues/8075
+    'ember-cli-uglify': {
+      uglify: {
+        compress: {
+          collapse_vars: false
+        }
+      }
+    }
+  });
 
   app.import('vendor/tailwind.min.css');
 
