f2b309b312020924a720a601204f48164e461242
Haochuan Liu <haochuan.liu@gmail.com>
2019-09-25 08:44:23 -0700

    Allow user to pass functions inside the  parameter while calling

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ let bodyParser = require('body-parser');
 let path = require('path');
 let fs = require('fs');
 let minimist = require('minimist');
+let JSONfn = require('json-fn');
 
 module.exports = {
   name: 'ember-cli-fastboot-testing',
@@ -81,7 +82,7 @@ module.exports = {
         response: {},
       };
 
-      let options = Object.assign(defaultOptions, req.body.options);
+      let options = Object.assign(defaultOptions, JSONfn.parse(req.body.options));
 
       res.set('x-fastboot-testing', true);
 
