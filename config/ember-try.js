0a77bbc8e976e36b7d276da044721a237db402b5
Ryan T <ryanto@gmail.com>
2020-01-15 16:13:45 -0500

    Fix Ember Data najax errors (#240)

--- config/ember-try.js
+++ config/ember-try.js
@@ -1,95 +1,101 @@
-'use strict';
+"use strict";
 
-const getChannelURL = require('ember-source-channel-url');
+const getChannelURL = require("ember-source-channel-url");
 
 module.exports = function() {
   return Promise.all([
-    getChannelURL('release'),
-    getChannelURL('beta'),
-    getChannelURL('canary')
-  ]).then((urls) => {
+    getChannelURL("release"),
+    getChannelURL("beta"),
+    getChannelURL("canary")
+  ]).then(urls => {
     return {
       useYarn: true,
       scenarios: [
         {
-          name: 'ember-lts-2.8',
-          bower: {
-            dependencies: {
-              'ember': 'components/ember#lts-2-8'
+          name: "ember-lts-2.18",
+          npm: {
+            devDependencies: {
+              "ember-source": "~2.18.0",
+              "ember-data": "~2.18.0"
+            }
+          }
+        },
+        {
+          name: "ember-lts-3.4",
+          npm: {
+            devDependencies: {
+              "ember-source": "~3.4.0",
+              "ember-data": "~3.4.0"
+            }
+          }
+        },
+        {
+          name: "ember-lts-3.8",
+          npm: {
+            devDependencies: {
+              "ember-source": "~3.8.0",
+              "ember-data": "~3.8.0"
+            }
+          }
+        },
+        {
+          name: "ember-lts-3.12",
+          npm: {
+            devDependencies: {
+              "ember-source": "~3.12.0",
+              "ember-data": "~3.12.0"
             },
             resolutions: {
-              'ember': 'lts-2-8'
-            }
-          },
-          npm: {
-            devDependencies: {
-              'ember-source': null,
-              'ember-cli-head': '0.3.1'
+              "ember-data": "~3.12.0"
             }
           }
         },
         {
-          name: 'ember-lts-2.18',
+          name: "fastboot-1.2",
           npm: {
             devDependencies: {
-              'ember-source': '~2.18.0'
+              "ember-source": "~3.4.0",
+              "ember-data": "~3.4.0",
+              fastboot: "~1.2.0"
             }
           }
         },
         {
-          name: 'ember-lts-3.4',
+          name: "ember-release",
           npm: {
             devDependencies: {
-              'ember-source': '~3.4.0'
+              "ember-source": urls[0],
+              // at the time of writing this the current 'latest' of ember-data is
+              // 3.15, which is broken in fastboot. we're going to use 3.13 since it's
+              // the closest working version
+              "ember-data": "~3.13.0"
+            },
+            resolutions: {
+              "ember-data": "~3.13.0"
             }
           }
         },
         {
-          name: 'ember-lts-3.8',
+          name: "ember-beta",
           npm: {
             devDependencies: {
-              'ember-source': '~3.8.0'
+              "ember-source": urls[1],
+              "ember-data": "beta"
+            },
+            resolutions: {
+              "ember-data": "beta"
             }
           }
         },
         {
-          name: 'ember-lts-3.12',
+          name: "ember-canary",
           npm: {
             devDependencies: {
-              'ember-source': '~3.12.0'
-            }
-          }
-        },
-        {
-          name: 'fastboot-1.2',
-          npm: {
-            devDependencies: {
-              'ember-source': '~3.4.0',
-              'fastboot': '~1.2.0'
-            }
-          }
-        },
-        {
-          name: 'ember-release',
-          npm: {
-            devDependencies: {
-              'ember-source': urls[0]
-            }
-          }
-        },
-        {
-          name: 'ember-beta',
-          npm: {
-            devDependencies: {
-              'ember-source': urls[1]
-            }
-          }
-        },
-        {
-          name: 'ember-canary',
-          npm: {
-            devDependencies: {
-              'ember-source': urls[2]
+              "ember-source": urls[2],
+              "ember-data": "canary"
+            },
+            resolutions: {
+              "ember-data": "canary"
             }
           }
         }
