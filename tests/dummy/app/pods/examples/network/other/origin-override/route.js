2aa02a0ffd1f5a0d6379c9e927fc461836d45268
Sergey Astapov <sergeastapov@gmail.com>
2021-10-12 23:02:22 -0400

    Modernize dummy app setup
    Switch to native classes.
    Fix deprecations in the latest Ember.js 3.x

--- tests/dummy/app/pods/examples/network/other/origin-override/route.js
+++ tests/dummy/app/pods/examples/network/other/origin-override/route.js
@@ -2,12 +2,12 @@ import Route from '@ember/routing/route';
 import fetch from 'fetch';
 import config from 'dummy/config/environment';
 
-export default Route.extend({
-
+export default class extends Route {
   async model() {
     const { originForOverride } = config;
-    let response = await fetch(`${originForOverride}/api/notes`, { method: 'get' });
+    let response = await fetch(`${originForOverride}/api/notes`, {
+      method: 'get',
+    });
     return await response.json();
   }
-
-});
+}
--- tests/dummy/app/pods/examples/visit-customization/route.js
+++ tests/dummy/app/pods/examples/visit-customization/route.js
@@ -1,15 +1,15 @@
 import Route from '@ember/routing/route';
 import { inject as service } from '@ember/service';
 
-export default Route.extend({
-  fastboot: service('fastboot'),
+export default class extends Route {
+  @service fastboot;
 
   model() {
     let { data, add } = this.fastboot.get('metadata');
 
     return {
       data,
-      add: add(1, 2)
+      add: add(1, 2),
     };
   }
-});
+}
