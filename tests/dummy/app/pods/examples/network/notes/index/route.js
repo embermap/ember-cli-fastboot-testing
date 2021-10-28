e22074ef826de08c97ecaf8d252c20d21dc8b60e
Sergey Astapov <sergeastapov@gmail.com>
2021-10-27 21:37:14 -0400

    Fix tests for Ember v4

--- tests/dummy/app/pods/examples/network/notes/index/route.js
+++ tests/dummy/app/pods/examples/network/notes/index/route.js
@@ -1,6 +1,9 @@
 import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
 
 export default class extends Route {
+  @service store;
+
   async model() {
     return this.store.findAll('note');
   }
--- tests/dummy/app/pods/examples/network/notes/note/route.js
+++ tests/dummy/app/pods/examples/network/notes/note/route.js
@@ -1,3 +1,10 @@
 import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
 
-export default class extends Route {}
+export default class extends Route {
+  @service store;
+
+  async model(params) {
+    return this.store.findRecord('note', params['note_id']);
+  }
+}
