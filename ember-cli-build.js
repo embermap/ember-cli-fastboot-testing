bedb95c125af01f54db2706918eda4999cf99e4b
Sergey Astapov <SergeAstapov@gmail.com>
2023-03-27 15:29:06 -0400

    Add polyfillUUID

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -5,6 +5,13 @@ const EmberAddon = require('ember-cli/lib/broccoli/ember-addon');
 module.exports = function (defaults) {
   const app = new EmberAddon(defaults, {
     // Add options here
+    '@embroider/macros': {
+      setConfig: {
+        '@ember-data/store': {
+          polyfillUUID: true,
+        },
+      },
+    },
   });
 
   app.import('vendor/tailwind.min.css');
