7bc00153ae9f6e7fe42ac86f3c324a9174d4e4d7
Sergey Astapov <sergeastapov@gmail.com>
2026-09-22 23:42:04 -0400

    ember-try 4.8 fixes

--- tests/dummy/config/ember-try.js
+++ tests/dummy/config/ember-try.js
@@ -11,7 +11,9 @@ module.exports = async function () {
         name: 'ember-lts-3.28',
         npm: {
           devDependencies: {
+            '@glimmer/component': '^1.1.2',
             'ember-cli': '~4.12.0',
+            'ember-resolver': '^11.0.0',
             'ember-source': '~3.28.0',
           },
         },
@@ -20,6 +22,8 @@ module.exports = async function () {
         name: 'ember-lts-4.4',
         npm: {
           devDependencies: {
+            '@glimmer/component': '^1.1.2',
+            'ember-resolver': '^11.0.0',
             'ember-source': '~4.4.0',
           },
         },
@@ -29,6 +33,7 @@ module.exports = async function () {
         npm: {
           devDependencies: {
             '@glimmer/component': '^1.1.2',
+            'ember-resolver': '^11.0.0',
             'ember-source': '~4.8.0',
           },
         },
